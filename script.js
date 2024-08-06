6e4345a6458102505914adec0aeed192dc558da4
Praneetha Bollineni <praneethabollineni@Praneethas-MacBook-Pro.local>
2024-08-06 17:22:43 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+console.log(menuIcon, navbar, sections, navLinks); // Log the selected elements
+
+window.onscroll = () => {
+    console.log('Scroll event triggered'); // Log when the scroll event is triggered
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active');
+            });
+        }
+    });
+};
+
+
+
+menuIcon.onclick = () => {
+    console.log('Click event triggered');
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+}
